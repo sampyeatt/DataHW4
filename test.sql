6b0c8edbf337982d7fdb11f2c3ac4f973fa63687
Sam Pyeatt <sampyeatt@gmail.com>
2020-03-20 12:47:04 -0600

    Finished... Maybe

--- test.sql
+++ test.sql
@@ -24,12 +24,12 @@ describe visitHistory;
 
 -- part 3
 
-insert into animalRecord (petID, ownerID, petName, breed, color, birthday, neuteredORspayed, timePercentageOutside, diet, animalType)
+insert into animalRecord (petID, ownerID, petName, breed, color, birthday, neuteredORspayed, timePercentageOutside, diet, animalType, gender)
 Values
-	('666', '1', 'Reggie', 'Lab', 'Black', '2000-09-22', True , 14, 'IAMS', 'Dog'),
-	('99', '3', 'Bagle', 'Border Colie', 'Black and White', '2017-06-02', True , 25, 'IAMS', 'Dog'),
-	('420', '9', 'Kitty', 'Russian Blue', 'Gray', '2005-04-15', True , 1, 'Fancy Feast', 'Cat'),
-	('42', '5', 'Momo', 'Mix', 'White', '2018-03-09', True , 2, 'Fancy Feast', 'Cat');
+	('666', '1', 'Reggie', 'Lab', 'Black', '2000-09-22', True , 14, 'IAMS', 'Dog', 'Male'),
+	('99', '3', 'Bagle', 'Border Colie', 'Black and White', '2017-06-02', True , 25, 'IAMS', 'Dog', 'Female'),
+	('420', '9', 'Kitty', 'Russian Blue', 'Gray', '2005-04-15', True , 1, 'Fancy Feast', 'Cat', 'Female'),
+	('42', '5', 'Momo', 'Mix', 'White', '2018-03-09', True , 2, 'Fancy Feast', 'Cat', 'Female');
 
 insert into ownerRecord (ownerID, ownerName, ownerNumber, ownerSign, signDate)
 values 
@@ -53,16 +53,25 @@ values
     (5, 666, True),
     (6, 99, True);
 
-insert into session (sessionID, trainCapacity, trainer, date, Training_sessionID, trainType_trainType)
+insert into session (sessionID, trainCapacity, trainer, date, time, Training_sessionID, trainType_trainType)
 values 
-	(2, 15, 'Shey', '2020-03-29', 1, 1),
-	(3, 13, 'Shey', '2020-04-28', 2, 2),
-	(4, 14, 'Marie', '2020-03-14', 3, 3),
-	(5, 17, 'Marie', '2020-03-17', 4, 3),
-	(6, 18, 'Shey', '2020-03-19', 5, 2),
-	(7, 23, 'Shey', '2020-03-21', 6, 1);
+	(2, 15, 'Shey', '2020-03-29', '10:00:00', 1, 1),
+	(3, 13, 'Shey', '2020-04-28', '11:00:00', 2, 2),
+	(4, 14, 'Marie', '2020-03-14', '12:00:00', 3, 3),
+	(5, 17, 'Marie', '2020-03-17', '13:00:00', 4, 3),
+	(6, 18, 'Shey', '2020-03-19', '14:00:00', 5, 2),
+	(7, 23, 'Shey', '2020-03-21', '15:00:00', 6, 1);
 
 delete from animalRecord
 	where petID = '666';
 
 SELECT * from animalRecord;
+
+SELECT O.ownerName, A.petID, A.petName, A.animalType, A.breed, A.gender, A.birthday
+	FROM animalRecord A, ownerRecord O
+    Where O.ownerID = A.ownerID;
+    
+SELECT S.date, S.time, S.trainer, T.type, O.ownerName, P.petName
+	FROM session S, trainType T, ownerRecord O, animalRecord P, training TR
+    where TR.sessionID = S.Training_sessionID and O.ownerID = P.ownerID and TR.animalRecord_petID = P.petID and S.trainType_trainType = T.trainType;
+    
